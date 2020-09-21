70fa1b5d04acf380d1ada383269b7adeafe2ee08
a.burdakovskaya <a.burdakovskaya@g.nsu.ru>
2020-09-21 22:55:24 +0700

    Lab 1: Project Euler #6 is completed.

---
+++ ProjectEuler_6/number6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sumOfSquares(int n) {
+	int sum = 0;
+	for (int i = 1; i <= n; i++) sum += i * i;
+	return sum;
+}
+
+int squareOfSum(int n) {
+	int sum = 0;
+	for (int i = 1; i <= n; i++) sum += i;
+	return sum * sum;
+}
+
+int difference(int a, int b) {
+	return a - b;
+}
+
+int main() {
+	int x = difference(squareOfSum(100), sumOfSquares(100));
+	printf("%d\n", x);
+	return 0;
+}
