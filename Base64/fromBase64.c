87d4bbacb2ac78a581429d7009a6ad68b61761b0
a.burdakovskaya <a.burdakovskaya@g.nsu.ru>
2020-11-18 21:28:26 +0700

    fromBase64.c decrypts equal signs.

--- Base64/fromBase64.c
+++ Base64/fromBase64.c
@@ -4,16 +4,23 @@
 
 const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const int formatNumber = 255;
+const int equalSignCode = 61;
 
-void decryp (int * input, size_t letterAmount, char output[3]){
+void decryp (int * input, char output[3]){
 	int origLetter[4];
 	origLetter[0] = strchr(alphabet, (char)input[0]) - alphabet;
 	origLetter[1] = strchr(alphabet, (char)input[1]) - alphabet;
-	origLetter[2] = strchr(alphabet, (char)input[2]) - alphabet;
-	origLetter[3] = strchr(alphabet, (char)input[3]) - alphabet;
 	output[0] = (char)((origLetter[0] << 2) | (origLetter[1] >> 4));
-	output[1] = (char)(((origLetter[1] << 4) & formatNumber) | (origLetter[2] >> 2));
-	output[2] = (char)(((origLetter[2] << 6) & formatNumber) | origLetter[3]);
+	if(input[2] != equalSignCode) {
+		origLetter[2] = strchr(alphabet, (char)input[2]) - alphabet;
+		output[1] = (char)(((origLetter[1] << 4) & formatNumber) | (origLetter[2] >> 2));
+		if(input[3] != equalSignCode) {
+			origLetter[3] = strchr(alphabet, (char)input[3]) - alphabet;
+			output[2] = (char)(((origLetter[2] << 6) & formatNumber) | origLetter[3]);
+		}
+	}
+	
+	
 }
 
 int main(int argc, char** argv){
@@ -25,32 +32,32 @@ int main(int argc, char** argv){
 	}
 	int currentLetters[4];
 
-	while(EOF != currentLetters[0]){
+	while(EOF != currentLetters[0] && equalSignCode != currentLetters[2] && equalSignCode != currentLetters[3]){
 		int letterAmount = 0;
 		currentLetters[0] = fgetc(inputFile);
 
 
 		if (EOF != currentLetters[0]){
-			letterAmount++;
 			currentLetters[1] = fgetc(inputFile);
 			
 			if (EOF != currentLetters[1]){
 				letterAmount++;
 				currentLetters[2] = fgetc(inputFile);
 				
-				if (EOF != currentLetters[2]){
+				if (EOF != currentLetters[2] && equalSignCode != currentLetters[2]){
 					letterAmount++;
 					currentLetters[3] = fgetc(inputFile);
 					
-					if (EOF != currentLetters[3]){
+					if (EOF != currentLetters[3] && equalSignCode != currentLetters[3]){
 						letterAmount++;
 					}
 				}
 			}
 			char output[3] = {0};
-			decryp(currentLetters, letterAmount, output);
-			//printf("%d %d %d %d\n", currentLetters[0], currentLetters[1], currentLetters[2], currentLetters[3]);
-			printf("%c %c %c ", output[0], output[1], output[2]);
+			decryp(currentLetters, output);
+			for (int i = 0; i < letterAmount; i++){
+				printf("%c", output[i]);
+			}
 			//fprintf(outputFile, "%s", output);
 		}
 	}
